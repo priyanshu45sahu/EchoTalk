f1169925a3acd3bc6f5154530256cb74fccbe625
Priyanshu Sahu <priyanshusahu.9047.com>
2025-01-17 18:59:34 +0530

    updated favicon meta data

--- EchoTalk/server/constants/config.js
+++ EchoTalk/server/constants/config.js
@@ -8,6 +8,6 @@ const corsOptions = {
   credentials: true,
 };
 
-const CHATTU_TOKEN = "chattu-token";
+const CHATAPP_TOKEN = "chatapp-token";
 
-export { corsOptions, CHATTU_TOKEN };
+export { corsOptions, CHATAPP_TOKEN };
--- EchoTalk/server/utils/features.js
+++ EchoTalk/server/utils/features.js
@@ -13,7 +13,7 @@ const cookieOptions = {
 
 const connectDB = (uri) => {
   mongoose
-    .connect(uri, { dbName: "Chattu" })
+    .connect(uri, { dbName: "Echotalk" })
     .then((data) => console.log(`Connected to DB: ${data.connection.host}`))
     .catch((err) => {
       throw err;
@@ -23,7 +23,7 @@ const connectDB = (uri) => {
 const sendToken = (res, user, code, message) => {
   const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
 
-  return res.status(code).cookie("chattu-token", token, cookieOptions).json({
+  return res.status(code).cookie("chatapp-token", token, cookieOptions).json({
     success: true,
     user,
     message,
